6b6c9b350352ad0850f0236c9a9d3302c3695bea
Rollen Gomes <avelliance@gmail.com>
2012-05-04 00:43:10 +0800

    inital commit

---
+++ spec/features/use_factory.spec.js
@@ -0,0 +1,27 @@
+require('../spec_helper');
+
+Given('I define a factory with a template object', function(){
+  var factoryService, 
+  instance, 
+  template,
+  database,
+  name;
+
+  beforeEach(function(){
+    factoryService = FactoryService(database);
+    name = 'job';
+    template = {'name':'rollen'};
+
+    factoryService.define(name, template);
+  });
+
+  When('I create an instance of the object', function(){
+    beforeEach(function(){
+      instance = factoryService.create('job');
+    });
+
+    Then('instance should be based on the template', function(){
+      expect(instance).toBe(template);
+    });
+  });
+});
